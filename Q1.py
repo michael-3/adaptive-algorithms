e363d7dd6e1d4f5b4d012e2d6f928a5c514d909a
Ian Minoso <iminoso@gmail.com>
2016-07-06 04:17:00 -0400

    Q1 Done

--- Q1.py
+++ Q1.py
@@ -6,28 +6,8 @@
 4 - Ending Point
 '''
 
-from Queue import Queue
-import heapq
-
-
-class PriorityQueue:
-    '''
-    priority queue for a* search
-    '''
-
-    def __init__(self):
-        self._queue = []
-        self._index = 0
-
-    def push(self, item, priority):
-        heapq.heappush(self._queue, (-priority, self._index, item))
-        self._index += 1
-
-    def pop(self):
-        return heapq.heappop(self._queue)[-1]
-
-    def empty(self):
-        return self._queue == []
+from Queue import Queue, PriorityQueue
+import copy
 
 
 def get_neighbors(arr, cur):
@@ -41,19 +21,19 @@ def get_neighbors(arr, cur):
     neighbors = []
 
     # top
-    if (r - 1) > -1:
+    if (r - 1) > -1 and arr[r - 1][c] != '1':
         neighbors.append((r - 1, c))
 
     # right
-    if (c + 1) < cols:
+    if (c + 1) < cols and arr[r][c + 1] != '1':
         neighbors.append((r, c + 1))
 
     # bottom
-    if (r + 1) < rows:
+    if (r + 1) < rows and arr[r + 1][c] != '1':
         neighbors.append((r + 1, c))
 
     # left
-    if (c - 1) > -1:
+    if (c - 1) > -1 and arr[r][c - 1] != '1':
         neighbors.append((r, c - 1))
 
     return neighbors
@@ -65,10 +45,12 @@ def get_path(d, start, end):
     path = list()
     val = d[e]
     path.append(end)
-    path.append(val)
+    coor = val.strip('()').split(',')
+    path.append((int(coor[0]), int(coor[1])))
     while val != s:
         val = d[val]
-        path.append(val)
+        coor = val.strip('()').split(',')
+        path.append((int(coor[0]), int(coor[1])))
     return path
 
 
@@ -93,10 +75,12 @@ def bfs(grid, start):
 
     while not fringe.empty():
         current = fringe.get()
-        print "Visiting at {}".format(current)
+
+        # print "Visiting at {}".format(current)
+
         if grid[current[0]][current[1]] == '4':
             print "Found {}".format(current)
-            print "Path = {}".format(get_path(path, start, current))
+            return get_path(path, start, current)
             break
 
         for n in get_neighbors(grid, current):
@@ -116,10 +100,12 @@ def dfs(grid, start):
 
     while fringe:
         current = fringe.pop()
-        print "Visiting at {}".format(current)
+
+        # print "Visiting at {}".format(current)
+
         if grid[current[0]][current[1]] == '4':
             print "Found {}".format(current)
-            print "Path = {}".format(get_path(path, start, current))
+            return get_path(path, start, current)
             break
 
         for n in get_neighbors(grid, current):
@@ -127,6 +113,15 @@ def dfs(grid, start):
                 path[str(n)] = str(current)
                 fringe.append(n)
                 visited[n] = True
+
+
+class a_cell:
+
+    def __init__(self, val):
+        self.val = val
+        self.h = 0
+        self.g = 0
+        self.parent = None
 
 
 def heuristic(current, next):
@@ -138,36 +133,80 @@ def heuristic(current, next):
     return a + b
 
 
+def in_queue(item, pq):
+    for i in pq.queue:
+        if i[1].val == item.val:
+            return True
+    return False
+
+
 def a_star(grid, start, end):
-    fringe = PriorityQueue()
-    fringe.push(start, 0)
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
-    path = {}
+    print "Starting at {}".format(start)
 
-    while not fringe.empty():
-        current = fringe.pop()
-        print "Visiting at {}".format(current)
+    start = a_cell(start)
+    end = a_cell(end)
 
-        if current == end:
-            print "Found {}".format(current)
-            print "Path = {}".format(get_path(path, start, current))
+    # Priority to sort queue on heuristic values
+    openset = PriorityQueue()
+    closedset = set()
+    openset.put((start.h + start.g, start))
+
+    while openset:
+        # Find the item in the open set with the lowest G + H score
+        current = openset.get_nowait()[1]
+
+        # print "Visiting at {}".format(current.val)
+
+        if current.val == end.val:
+            print "Found {}".format(current.val)
+            path = []
+            while current.parent:
+                path.append(current.val)
+                current = current.parent
+            path.append(current.val)
+            return path
             break
 
-        for n in get_neighbors(grid, current):
-            new_cost = cost_so_far[current] + heuristic(current, n)
-            if n not in cost_so_far or new_cost < cost_so_far[n]:
-                cost_so_far[n] = new_cost
-                priority = new_cost + heuristic(end, n)
-                fringe.push(n, priority)
-                came_from[n] = current
-                path[str(n)] = str(current)
+        # Add it to the closed set
+        closedset.add(current)
 
+        for neighbor in get_neighbors(grid, current.val):
+
+            cell = a_cell(neighbor)
+
+            # If it is already in the closed set, skip it
+            if cell in closedset:
+                continue
+
+            # Otherwise if it is already in the open set
+            if in_queue(cell, openset):
+                # Check if we beat the G score
+                new_g = current.g + 1
+                if cell.g > new_g:
+                    # If so, update the cell to have a new parent
+                    cell.g = new_g
+                    cell.parent = current
+            else:
+                # Not in open set, calculate the G and H score for cell
+                cell.g = current.g + 1
+                cell.h = heuristic(cell.val, end.val)
+
+                # Set the parent to our current item
+                cell.parent = current
+
+                # Add it to the set
+                openset.put((cell.h + cell.g, cell))
+
+
+def print_grid(grid, path):
+    g = copy.deepcopy(grid)
+    for p in path:
+        g[p[0]][p[1]] = ' '
+    g[path[0][0]][path[0][1]] = 'x'
+    g[path[len(path) - 1][0]][path[len(path) - 1][1]] = 'x'
+    return g
 
 files = ['maze_e1.txt', 'maze_e2.txt', 'maze_e3.txt']
-# files = ['maze_e1.txt']
 
 for f in files:
     lines = [line.rstrip('\n') for line in open('mazes/' + f)]
@@ -179,12 +218,29 @@ for f in files:
     start = find(grid, '3')
     end = find(grid, '4')
 
-    print "Starting BFS for {}".format(f)
+    name = 'mazes/output_bfs_' + f
+    out = open(name, 'w+')
+    print "\nStarting BFS for {}".format(f)
     print "==================="
-    bfs(grid, start)
-    print "Starting DFS for {}".format(f)
+    path = bfs(grid, start)
+    print >>out, "Path = {}\n".format(path)
+    for row in print_grid(grid, path):
+        print >>out, row
+
+    name = 'mazes/output_dfs_' + f
+    out = open(name, 'w+')
+    print "\nStarting DFS for {}".format(f)
     print "==================="
-    dfs(grid, start)
-    print "Starting A_* for {}".format(f)
+    path = dfs(grid, start)
+    print >>out, "Path = {}\n".format(path)
+    for row in print_grid(grid, path):
+        print >>out, row
+
+    name = 'mazes/output_astar_' + f
+    out = open(name, 'w+')
+    print "\nStarting A_* for {}".format(f)
     print "==================="
-    a_star(grid, start, end)
+    path = a_star(grid, start, end)
+    print >>out, "Path = {}\n".format(path)
+    for row in print_grid(grid, path):
+        print >>out, row
