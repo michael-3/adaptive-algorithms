66f8d48ad0f2f130adbefc521b2f8a5bc71db26c
michael-3 <hth.michael@gmail.com>
2016-07-08 13:57:05 -0400

    q5 cost function

--- Q5.py
+++ Q5.py
@@ -27,17 +27,17 @@ M0 = M0;
 Return(BestS);
 }
 '''
-
+import math
 # Parser
 # return hashmap of capacity for each node, list of customers, hashmap of
 # coordinates for each node
 
 
 # customers - array of tuples (customer, demand)
-
+# return: hashmap of routes {route #: route}
 
 def initialSolution(customers, capacity):
-    routes = {}
+    solution = {}
     visited = []
     while len(visited) != len(customers):
         c = capacity
@@ -48,12 +48,8 @@ def initialSolution(customers, capacity):
                 visited.append(customer[0])
                 c -= customer[1]
         route.append(0)
-        routes[len(routes)] = route
-    return routes
-
-cust = [(2, 3), (3, 9), (4, 6), (5, 5), (6, 7), (7, 12), (8, 15), (9, 3)]
-
-print initialSolution(cust, 15)
+        solution[len(solution)] = route
+    return solution
 
 
 def annealingCVRP(customers, capacity, vehicles):
@@ -66,9 +62,33 @@ def annealingCVRP(customers, capacity, vehicles):
     current_s = s
     current_cost = cost(s)
 
+# solution - hashmap of routes {route #: route}
+# coordinates - hashmap of coordinates {node 1: (x,y)}
+# return: total cost of solution
 
-def cost(solution):
+
+def cost(solution, coordinates):
+    total_cost = 0
+    for route in solution.values():
+        current = 0
+        next = 1
+        while next < len(route):
+            node1 = coordinates.get(route[current])
+            node2 = coordinates.get(route[next])
+            total_cost += euclid_distance(node1[0],
+                                          node1[1], node2[0], node2[1])
+            current = next
+            next += 1
+    return total_cost
 
 
 def euclid_distance(x1, y1, x2, y2):
-    return math.sqrt((x2 - x1) ** 2 - (y2 - y1) ** 2)
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+cust = [(2, 3), (3, 9), (4, 6), (5, 5), (6, 7), (7, 12), (8, 15), (9, 3)]
+loc = {0: (0, 0), 1: (82, 76), 2: (96, 44), 3: (50, 5), 4: (49, 8),
+       5: (13, 7), 6: (29, 89), 7: (58, 30), 8: (84, 39), 9: (14, 24)}
+
+s = initialSolution(cust, 15)
+
+print cost(s, loc)
