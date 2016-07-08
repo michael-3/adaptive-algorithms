634be7b43b8f7c3c5f6d6067f359e0f7f6ffe0b8
michael-3 <hth.michael@gmail.com>
2016-07-07 23:59:35 -0400

    added Q5 initial solution

--- Q5.py
+++ Q5.py
@@ -1,7 +1,7 @@
 '''
 Annealing CVRP()
 { S0 = Initial solution
- ↵ = 0.99
+ = 0.99
   = 1.05
 M0 = 5
 BestS = Best solution T = 5000
@@ -21,9 +21,30 @@ CurrentS=NewS
 CurrentCost = Cost(CurrentS); M=M 1
 //Temperature reduction multiplier // Iteration multiplier
 //Time until next parameter update
- } while (M   0) Time = Time + M0; T = ↵ ⇤T;
-M0 = ⇤M0;
+ } while (M   0) Time = Time + M0; T =T;
+M0 = M0;
 } while (Time > MaxTime and T > 0.001);
 Return(BestS);
 }
 '''
+
+# customers - array of tuples (customer, demand)
+
+
+def initialSolution(customers, capacity):
+    routes = []
+    visited = []
+    while len(visited) != len(customers):
+        c = capacity
+        route = [0]
+        for customer in customers:
+            if customer[1] <= c and customer[0] not in visited:
+                route.append(customer[0])
+                visited.append(customer[0])
+                c -= customer[1]
+        routes += route
+    return routes
+
+cust = [(2, 3), (3, 9), (4, 6), (5, 5), (6, 7), (7, 12), (8, 15), (9, 3)]
+
+print initialSolution(cust, 15)
